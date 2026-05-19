ad19c0010c572910c739f5ca2cc73d1b75c2e935
BogdanATA <batameev@gmail.com>
2026-05-19 10:17:25 -0400

    Created `Person` class and generated a list of 10 people

--- src/main/java/com/pluralsight/loops/Program.java
+++ src/main/java/com/pluralsight/loops/Program.java
@@ -1,4 +1,29 @@
 package com.pluralsight.loops;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Person> people = new ArrayList<>();
+
+        people.add(new Person("John", "Smith", 25));
+        people.add(new Person("Sarah", "Johnson", 32));
+        people.add(new Person("Michael", "Brown", 18));
+        people.add(new Person("Emily", "Davis", 41));
+        people.add(new Person("David", "Wilson", 29));
+        people.add(new Person("Jessica", "Miller", 35));
+        people.add(new Person("Daniel", "Taylor", 22));
+        people.add(new Person("Ashley", "Anderson", 27));
+        people.add(new Person("Chris", "Thomas", 50));
+        people.add(new Person("Amanda", "Moore", 19));
+
+
+
+
+
+    }
 }
